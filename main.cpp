21060880331ad9a7694afea631cb1aeda3ad1a8c
theoarba <amhd.theo@gmail.com>
2017-06-19 21:23:13 +0700

    edisi pertama

---
+++ main.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+
+using namespace std;
+
+class Shape{
+    public:
+    void setHeight(int h){
+        Height = h;
+    }
+
+    void setWidth(int w){
+        Width = w;
+    }
+
+    protected:
+
+    int Height;
+    int Width;
+};
+
+
+class Retanggle: public Shape{
+    public:
+    int getArea (){
+        return (Height * Width);
+    }
+};
+
+int main()
+{
+    Retanggle rec;
+
+    rec.setHeight(10);
+    rec.setWidth(43);
+
+    cout<<rec.getArea();
+    return 0;
+}
